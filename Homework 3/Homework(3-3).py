d8411b911dee5ea7596bc409936676d7410d23e0
Ilya <ilya.totscki@yandex.ru>
2021-11-23 00:20:00 +0300

    Homework 3

--- Homework 3/Homework(3-3).py
+++ Homework 3/Homework(3-3).py
@@ -1,7 +1,7 @@
 def fib(n):
     a, b = 0, 1
     for i in range(n):
-        print(a, end = ' ')      #end = '' пишет все а в одну строку, без этого было в столбик
+        print(a, end = ' ')
         a, b = b, a + b
 n = int(input("Введите кол-во  "))
 fib(n)
