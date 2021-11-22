3c7eedcef44e5b8f03639ac9a04fe92c809863b5
MrFixMan01 <90065755+MrFixMan01@users.noreply.github.com>
2021-11-23 00:25:39 +0300

    Delete Homework(3-5).py

--- Homework 3/Homework(3-5).py
+++
@@ -1,9 +0,0 @@
-def sred(b):
-    s = sum(b) / len(b)
-    print("Среднее арифметическое =", s)
-n = input("Ввод ")
-b = list()
-while n != '':
-    b.append(float(n))
-    n = input("Ввод ")
-sred(b)
