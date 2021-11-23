d5a85363d723e29e7f42a6c3a94a332bf8594959
Ilya <ilya.totscki@yandex.ru>
2021-11-23 09:33:57 +0300

    Homework 4

--- Homework 4/Homework(4-2).py
+++ Homework 4/Homework(4-2).py
@@ -1,6 +1,6 @@
 def factorial(n):
     x = 1
-    for i in range(2, n + 1):
+    for i in range(2, n+1):
         x *= i
     return x
 n = int(input("Введите число "))
--- Homework 4/Homework(4-3).py
+++ Homework 4/Homework(4-3).py
@@ -4,10 +4,10 @@ from hm31 import func
 def f2(b):
     lst1 = list(set(b))
     if b == lst1:
-        return "True"
+        return True
     else:
-        return "False"
+        return False
 b = func()
 print(f2(b))
-assert f2([1, 2, 3, 3]) == "False"
-assert f2([1, 2, 3, 4, 5]) == "True"
+assert f2([1, 2, 3, 3]) == False
+assert f2([1, 2, 3, 4, 5]) == True
