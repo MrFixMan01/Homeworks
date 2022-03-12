fd62da3eee1f16cb54c1bb34207b20c4b92615d1
Ilya <ilya.totscki@yandex.ru>
2022-03-12 13:05:01 +0300

    Homework (2)

---
+++ Matrix/matrix2x2.py
@@ -0,0 +1,15 @@
+from matrixtest import Matrix
+
+
+class Matrix2x2(Matrix):
+    def __init__(self, matrix=[[0, 0], [0, 0]]):
+        assert len(matrix) == 2
+        assert len(matrix[0]) == 2
+        assert len(matrix[1]) == 2
+
+        self.strok = 2
+        self.stolb = 2
+        self.matrix = matrix
+
+    def determinant(self):
+        return self.matrix[0][0] * self.matrix[1][1] - self.matrix[0][1] * self.matrix[1][0]
---
+++ Matrix/tests.py
@@ -0,0 +1,7 @@
+from matrix2x2 import Matrix2x2
+
+
+m = Matrix2x2([[1, 2], [3, 4]])
+assert m.determinant() == -2
+m = Matrix2x2([[0, 0], [0, 0]])
+assert m.determinant() == 0
